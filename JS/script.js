77fd4c0d48196eb2a07a66121b3f6ed8d588a378
Sumit Deshar <sumeetdsr@gmail.com>
2024-02-02 16:25:59 +0545

    final commit of portfolio website(not connected to backend)

---
+++ JS/script.js
@@ -0,0 +1,7 @@
+// typing Animation
+var typed = new Typed(".typing", {
+    strings: ["Computer Engineer","Python Developer","Backend Developer", "Mobile Application Developer"],
+    typeSpeed: 50,
+    BackSpeed: 40,
+    loop: true
+})
